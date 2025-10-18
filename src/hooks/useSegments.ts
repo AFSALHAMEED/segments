1dd15e70c7b4264fc91abffc84a12d0af2f5913f
afsal.vh <afsalafsu821@gmail.com>
2025-10-18 19:24:04 +0530

    env updated

--- src/hooks/useSegments.ts
+++ src/hooks/useSegments.ts
@@ -13,7 +13,8 @@ type Props = {
 };
 
 export const useSegments = ({ onClose, showPopup }: Props) => {
-  const VITE_API_URL = import.meta.env.VITE_API_URL;
+  const VITE_API_URL =
+    import.meta.env.VITE_API_URL || "Ff7e95f29-f67b-4976-8746-5b78e7bab191";
 
   const schemaOptions = [
     { label: "First Name", value: "first_name" },
